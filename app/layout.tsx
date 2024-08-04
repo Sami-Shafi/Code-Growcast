9c1dadf2779b91aaa27c9b1778c27d8e7a41aa1c
sami-shafi <freelancersami123@gmail.com>
2024-08-04 06:12:49 +0600

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,19 +5,22 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "GrowCast - Modern AI Podcast",
+	title: {
+		template: "%s | GrowCast",
+		default: "GrowCast - Modern AI Podcast",
+	},
 	description:
-		"Growcast is a platform where you can explore and discover the latest prodcasts of popular creators and enrich your knowledge. Join us and access a vast library in a modern app UI and use features like AI Transcriptions, AI Summaries, and more.",
+		"Growcast is a platform where you can explore and discover the latest prodcasts of popular creators and enrich your knowledge. Join us and access a vast library in a modern app UI and use features like AI Transcriptions, AI Summaries, and more."
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
